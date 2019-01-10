37af45683183d24a603912f00d1b0f493a73923b
A Glazer <avigayilglazer@gmail.com>
2019-01-10 19:44:32 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +8,14 @@ def second_challenge
   }
 
   #code your solution here!
+  groceries.values do |list, data|
 
-  
+    data.each do |category, ingredients|
 
+
+
+
+    end.first
+
+  end
 end
