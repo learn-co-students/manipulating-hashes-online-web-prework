9e6a1353d4c13d91c0cbd513128244dbe7c0e973
Cody Yantis <cyantis@gmail.com>
2019-01-29 23:39:40 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,9 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
-
-  
-
+  new_arr = []
+  groceries.each do |k, v|
+    new_arr << v
+  end
+  new_arr.flatten
 end
