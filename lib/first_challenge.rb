a97659441c8ad668d35556daec0bc9c8a7501f12
kyle smith <kylemclellan@gmail.com>
2019-02-17 18:12:40 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,13 @@ def first_challenge
   }
 
   #your code here
-
-
+  contacts.each do |person, data| 
+    data.each do |attribute, data|
+      if attribute == :favorite_icecream_flavors
+        data.delete_if { |value| value == "strawberry" }
+      end
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
