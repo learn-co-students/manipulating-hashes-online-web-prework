69ea9de03483b14559861b89981748a6146e1fed
David Stewart <davidstewart@gmail.com>
2019-03-06 00:11:21 +0000

    Done.

---
+++ solution.rb
@@ -0,0 +1,41 @@
+
+def first_challenge
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+      knows: nil
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
+
+  contacts["Freddy Mercury"].each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+      end
+    end
+  end
+  #remember to return your newly altered contacts hash!
+  contacts
+end
+
+
+
+def second_challenge
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
+
+  groceries.values.flatten
+  
+
+end
