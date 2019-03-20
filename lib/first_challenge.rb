760fc32d2e00e5a06ef1ea7ae764b6f676b77a6c
Ethan <ethansagin@gmail.com>
2019-03-20 20:52:29 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,13 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, hash_data|
+    hash_data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if {|flavor| flavor == "strawberry"}
+      end
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
