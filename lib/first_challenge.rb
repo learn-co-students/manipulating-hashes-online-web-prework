7a730ffbc67dd265b2a4255007427f5e4444dd18
Jessica Hong <jessicahong.24@gmail.com>
2019-05-02 07:09:39 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,16 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
+contacts.each do |person,data|
+  data.each do |attributes, values|
+    if attributes == :favorite_icecream_flavors
+      values.each do|flavor|
+      flavor.delete!("strawberry")
+    end
+  end
+end
+  
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
