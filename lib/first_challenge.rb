1299939790a92c5bff29b1207788d68dc4270221
Blake Rose <bdecktor@gmail.com>
2019-05-10 18:21:59 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,9 +17,10 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.delete_if do |flavor|
           flavor == "strawberry"
+        end
       end
     end
-    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
