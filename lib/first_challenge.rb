8692e97465547f6770dec8ee06d62de8cdf5fad0
Brittany Javalera <brittjavalera@gmail.com>
2019-05-15 03:57:27 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,11 @@
       data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
           value.delete_if {|flavor| flavor == "strawberry"}
+          
+          #or contacts["Freddy Mercury"].each do |attribute, value|
+              #if attribute == :favorite_icecream_flavors
+              #value.delete_if do |flavor|
+              #flavor == "strawberry"
         end
       end
     end
@@ -26,3 +31,8 @@
   contacts
 end
 end
+
+#
+
+
+
