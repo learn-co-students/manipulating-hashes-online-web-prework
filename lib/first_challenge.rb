39fda014c462beb214221eae1c97d4e4b994757e
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 21:49:54 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,9 @@
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,9 +15,13 @@ def first_challenge
   }
 
   #your code here
-
-
+contacts["Freddy Mercury"][:favorite_icecream_flavors] = contacts["Freddy Mercury"][:favorite_icecream_flavors].map.with_index do |flavor, index|
+    if flavor == "strawberry"
+      contacts["Freddy Mercury"][:favorite_icecream_flavors][index] == "raspberry"
+    else
+      puts "not strawberry, instead #{flavor}"
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
