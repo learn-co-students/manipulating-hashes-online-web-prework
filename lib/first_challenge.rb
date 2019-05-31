195650bec95043c33d30d6a6fb9bf35c3c824481
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 22:14:26 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,14 +14,28 @@ def first_challenge
     }
   }
 
-  #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors] = contacts["Freddy Mercury"][:favorite_icecream_flavors].map.with_index do |flavor, index|
-    if flavor == "strawberry"
-      contacts["Freddy Mercury"][:favorite_icecream_flavors][index] == "raspberry"
-    else
-      puts "not strawberry, instead #{flavor}"
+#My Alternate Answer
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if value.class == Array
+      value.map.with_index do |flavor, index|
+        if flavor == "strawberry"
+          value.delete_at(index)
+        end
+      end
     end
   end
+end
+
+
+#My Original Answer
+#  contacts["Freddy Mercury"][:favorite_icecream_flavors] = contacts["Freddy Mercury"][:favorite_icecream_flavors].map.with_index do |flavor, index|
+#      if flavor == "strawberry"
+#        contacts["Freddy Mercury"][:favorite_icecream_flavors][index] == "raspberry"
+#      else
+#        puts "not strawberry, instead #{flavor}"
+#      end
+#  end
   #remember to return your newly altered contacts hash!
   contacts
 end
